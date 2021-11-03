0264be8d981edc098b61967f79ce617af1a43166
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-11-03 09:15:53 -0500

    Category List

--- newsApp/news_app/lib/src/services/news_service.dart
+++ newsApp/news_app/lib/src/services/news_service.dart
@@ -1,5 +1,7 @@
 // apikey 0e17d79662db4613b42913697f7db4f2
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:news_app/src/models/newsModels/category.dart';
 import 'package:news_app/src/models/newsModels/news.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,7 +10,15 @@ final _apiKey = "0e17d79662db4613b42913697f7db4f2";
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
-
+  List<Category> categoies = [
+    Category(FontAwesomeIcons.building, "business"),
+    Category(FontAwesomeIcons.tv, "entertainment"),
+    Category(FontAwesomeIcons.addressCard, "general"),
+    Category(FontAwesomeIcons.headSideVirus, "health"),
+    Category(FontAwesomeIcons.vials, "science"),
+    Category(FontAwesomeIcons.volleyballBall, "sports"),
+    Category(FontAwesomeIcons.memory, "technology")
+  ];
   NewsService() {
     getTopHeadlines();
   }
